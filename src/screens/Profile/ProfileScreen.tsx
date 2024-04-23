4ce0986a648d29a3670d39de704c888702c743da
HopeAdler <buihoangphucthd@gmail.com>
2024-04-23 23:59:52 +0700

    change profile screen

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,14 +1,24 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { FontAwesome, MaterialIcons, Ionicons } from "@expo/vector-icons"; // Import Ionicons
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import TextComponent from "../../components/Global/TextComponent";
 import ButtonComponent from "../../components/button/ButtonComponent";
-import { FontAwesome } from "@expo/vector-icons";
 import { appColors } from "../../constants/appColors";
 
 const ProfileScreen = () => {
   const auth = useAppSelector((state) => state.auth);
-  // console.log(auth.currentUser.photo);
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedGender, setSelectedGender] = useState("");
+
+  const handleEditPress = () => {
+    setIsEditing(true);
+  };
+
+  const handleSavePress = () => {
+    setIsEditing(false);
+    // Here you can handle saving the edited profile information
+  };
 
   return (
     <>
@@ -27,10 +37,10 @@ const ProfileScreen = () => {
       </View>
       <View
         style={{
-          zIndex:1,
-          position:'absolute',
-          top:200,
-          left:50,
+          zIndex: 1,
+          position: "absolute",
+          top: 200,
+          left: 50,
           flex: 1,
           justifyContent: "flex-start",
           alignContent: "center",
@@ -43,11 +53,13 @@ const ProfileScreen = () => {
               auth.currentUser.photo ||
               "https://static.wikia.nocookie.net/houkai-star-rail/images/2/22/Profile_Picture_Sparkle_-_Illusion.png/revision/latest/thumbnail/width/360/height/360?cb=20240206032940",
           }}
-          style={{ borderRadius: 9999, width: 100, height: 100,
-
-            borderWidth:5,
-            borderColor:appColors.primary
-           }}
+          style={{
+            borderRadius: 9999,
+            width: 100,
+            height: 100,
+            borderWidth: 5,
+            borderColor: appColors.primary,
+          }}
         />
         <TextComponent text={auth.currentUser.name || "User"} title />
         <TextComponent text={auth.currentUser.email || "User Email"} title />
@@ -55,17 +67,103 @@ const ProfileScreen = () => {
           text={auth.currentUser.familyName || "Family Name"}
           title
         />
-        {/* <TextComponent
-          text={auth.currentUser.givenName || "Family Name"}
-          title
-        /> */}
-        <ButtonComponent
-          iconFlex="right"
-          styles={{ width: 170 }}
-          icon={<FontAwesome name="edit" size={20} color={appColors.white} />}
-          text="Edit Profile"
-          type="primary"
-        />
+        {isEditing ? (
+          <>
+            {/* Editable fields */}
+            <View style={styles.inputContainer}>
+              <MaterialIcons
+                name="person-outline"
+                size={24}
+                color="black"
+                style={styles.icon}
+              />
+              <View style={styles.inputWrapper}>
+                <Text style={styles.labelText}>Name:</Text>
+                <Text style={styles.inputText}>
+                  {auth.currentUser.name || "User"}
+                </Text>
+                <View style={styles.underline}></View>
+              </View>
+            </View>
+            {/* Gender Selection */}
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="people"
+                size={24}
+                color="black"
+                style={styles.icon}
+              />
+              <Text style={styles.labelText}>Gender:</Text>
+              <TouchableOpacity
+                style={styles.radio}
+                onPress={() => setSelectedGender("male")}
+              >
+                <Text style={styles.radioText}>Male</Text>
+                <View
+                  style={[
+                    styles.radioCircle,
+                    {
+                      backgroundColor:
+                        selectedGender === "male" ? appColors.primary : "#fff",
+                    },
+                  ]}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.radio}
+                onPress={() => setSelectedGender("female")}
+              >
+                <Text style={styles.radioText}>Female</Text>
+                <View
+                  style={[
+                    styles.radioCircle,
+                    {
+                      backgroundColor:
+                        selectedGender === "female"
+                          ? appColors.primary
+                          : "#fff",
+                    },
+                  ]}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.radio}
+                onPress={() => setSelectedGender("other")}
+              >
+                <Text style={styles.radioText}>Other</Text>
+                <View
+                  style={[
+                    styles.radioCircle,
+                    {
+                      backgroundColor:
+                        selectedGender === "other" ? appColors.primary : "#fff",
+                    },
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            {/* Save button */}
+            <ButtonComponent
+              iconFlex="right"
+              styles={{ width: 170 }}
+              icon={
+                <FontAwesome name="save" size={20} color={appColors.white} />
+              }
+              text="Save"
+              type="primary"
+              onPress={handleSavePress}
+            />
+          </>
+        ) : (
+          <ButtonComponent
+            iconFlex="right"
+            styles={{ width: 170, marginTop: 10 }}
+            icon={<FontAwesome name="edit" size={20} color={appColors.white} />}
+            text="Edit Profile"
+            type="primary"
+            onPress={handleEditPress}
+          />
+        )}
       </View>
     </>
   );
@@ -73,4 +171,59 @@ const ProfileScreen = () => {
 
 export default ProfileScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+    marginHorizontal: -32,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  labelText: {
+    fontSize: 19,
+    marginRight: 10,
+    fontWeight: "bold",
+  },
+  inputWrapper: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderColor: "black",
+  },
+  inputText: {
+    fontSize: 20,
+    paddingBottom: 3,
+  },
+  underline: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 1,
+    backgroundColor: "black",
+  },
+  icon: {
+    marginRight: 10,
+  },
+  radioContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  radio: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 20,
+  },
+  radioText: {
+    marginRight: 5,
+    fontSize: 17,
+  },
+  radioCircle: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: appColors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
